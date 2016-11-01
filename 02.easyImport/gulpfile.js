8add9e651a87ac36713c2cdfde5aa9c7e69821a2
Takeshi Takatsudo <takazudo@gmail.com>
2016-11-01 17:03:27 +0900

    デモ調整

--- 02.easyImport/gulpfile.js
+++ 02.easyImport/gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
+// PostCSSのプラグイン
 var postcss = require("gulp-postcss");
 var simpleVars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var easyImport = require("postcss-easy-import");
+// その他
 var notify = require("gulp-notify");
 
 gulp.task("compileCss", function() {
@@ -12,14 +14,16 @@ gulp.task("compileCss", function() {
     simpleVars,
     nested,
   ];
-  return gulp.src("src/styles.css")
-    .pipe(postcss(processors))
-    .pipe(gulp.dest("./dest"))
-    .pipe(notify("CSS compiled."));
+  return gulp.src("src/styles.css") // CSSファイル全てが対象
+    .pipe(postcss(processors)) // PostCSSに渡して処理させる
+    .pipe(gulp.dest("./dest")) // destに出力
+    .pipe(notify("CSS compiled.")); // 通知
 });
 
+// CSSファイルの変更を検知してcompileCssを実行
 gulp.task("watch", function () {
   gulp.watch("src/**/*.css", ["compileCss"]);
 });
 
+// 実行時にはcompileCssしてwatch
 gulp.task("default", ["compileCss", "watch"]);
